e078f43cb7c586a080ac086bcfccac5d274f9de9
ShivomSehgal <sehgalshivom@gmail.com>
2024-05-07 10:52:38 +0530

    update entities

---
+++ entity/AccountStatus.java
@@ -0,0 +1,5 @@
+package entity;
+
+public enum AccountStatus {
+    ACTIVE, BLOCKED, BANNED, ARCHIVED, UNKNOWN
+}
---
+++ entity/ParkingSpaceType.java
@@ -0,0 +1,5 @@
+package entity;
+
+public enum ParkingSpaceType {
+    ELECTRIC, COMPACT, LARGE, MOTORBIKE, HANDICAPPED
+}
